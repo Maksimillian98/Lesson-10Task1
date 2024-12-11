f75c69df0a1bfcc498ac8c84b48a5ffcaaad33b4
maxstar <maksimgavura78@gmail.com>
2024-12-11 10:09:12 +0200

    Добавьте файлы проекта.

---
+++ Lesson#10Task1/Lesson_10Task1.py
@@ -0,0 +1,13 @@
+﻿pets = {
+    'Имя питомца: ':{}}
+pet_name = str(input("Введите имя питомца: "))
+pet_type = str(input("Введите вид питомца: "))
+pet_age = int(input("Введите возраст питомца: "))
+owner_name = str(input("Введите имя владелца: "))
+
+pets[pet_name] = {}
+pets[pet_name]['Вид питомца:'] = pet_type
+pets[pet_name]['Возраст питомца:'] = pet_age
+pets[pet_name]['Имя владельца'] = owner_name
+
+print(pets)
